036cb6b88027cdf4f1b1a670a6c4c5446c44f93b
Andrii Tokar <andrii.tokar@petuum.com>
2019-10-10 13:13:56 +0300

    create vue simple project

---
+++ src/components/Description.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div v-if="data">
+            <h3>Name:{{data.name}}</h3>
+            <p>Attempts: {{data.attempts}}</p>
+        </div>
+        <div v-if="!data">
+            please select any data item from list above!
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Description",
+        props: {
+            data: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/dataList.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in items" v-bind:key="item.id" v-on:click="getItem(item)">
+                {{item.name}}
+            </li>
+        </ul>
+        <Description v-bind:data="activeItem" />
+    </div>
+</template>
+
+<script>
+
+    import Description from "@/components/Description";
+
+    export default {
+        name: "DataList",
+        components: {
+           Description
+        },
+        data: function() {
+        return {
+            activeItem: {
+                id: 0,
+                name: '',
+                attempts: 0
+            },
+            items:  [
+                {
+                    id: "1",
+                    name: "abc",
+                    attempts: "7"
+                },
+                {
+                    id: "2",
+                    name: "bed",
+                    attempts: "6"
+                },
+                {
+                    id: "3",
+                    name: "efg",
+                    attempts: "9"
+                },
+                {
+                    id: "4",
+                    name: "hij",
+                    attempts: "2"
+                }
+            ]};
+        },
+        methods: {
+            getItem: function(item) {
+                this.activeItem = item;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
